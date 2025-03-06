efdbd01c191ed2ebc62587c8919afc0819fe5372
Ciiruu <ricardho.gunawan@gmail.com>
2025-03-06 21:05:41 +0700

    Refactor Dockerfile and docker-compose.yml for Laravel project with Node.js and Nginx

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,30 @@
-FROM php:8.3.18RC1-fpm-alpine3.21
+FROM node:18-alpine AS tailwind
+WORKDIR /var/www
 
-# Install dependensi yang dibutuhkan
-RUN apk add --no-cache icu-dev
+COPY . .
+RUN npm install && npm run build
 
-# Install ekstensi PHP yang dibutuhkan Laravel
-RUN docker-php-ext-configure intl && docker-php-ext-install intl pdo pdo_mysql
+
+FROM php:8.3.1-fpm-alpine3.19
+
+# dependensi 
+RUN apk add --no-cache icu-dev zip unzip libzip-dev 
+
+# ekstensi Laravel
+RUN docker-php-ext-configure zip && docker-php-ext-configure intl && docker-php-ext-install intl zip pdo pdo_mysql
+RUN wget https://github.com/composer/composer/releases/download/2.8.6/composer.phar -O /composer
+ RUN chmod +x /composer
+
 
 # Set working directory
 WORKDIR /var/www
 
-# Copy project Laravel ke dalam container
 COPY . .
 
-# Set permission untuk Laravel
+COPY --from=tailwind /var/www/public/build /var/www/public/build
+RUN /composer install
+
+# et permission Laravel
 RUN chmod -R 777 storage bootstrap/cache
 
 CMD ["php-fpm"]
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -18,7 +18,7 @@
 
 @section('content')
     <!-- Hero Section with Parallax Effect -->
-    <div class="relative overflow-hidden bg-gradient-to-r from-blue-600 to-purple-500 h-[600px] flex items-center">
+    <div class="relative overflow-hidden bg-gradient-to-r from-blue-600 to-purple-850 h-[600px] flex items-center">
         <div class="absolute inset-0">
             <div class="absolute inset-0 bg-pattern opacity-5"
                 style="background-image: url('data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23ffffff' fill-opacity='0.3'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E');">
@@ -157,7 +157,7 @@
 
     <!-- Call to Action -->
     <div id="contact" class="container mx-auto px-4 py-12">
-        <div class="bg-gradient-to-r from-blue-600 to-purple-500 rounded-xl shadow-xl p-8 text-white text-center"
+        <div class="bg-gradient-to-r from-blue-600 to-purple-850 rounded-xl shadow-xl p-8 text-white text-center"
             data-aos="fade-up">
             <h2 class="text-3xl font-bold mb-4">Ready to Start Your Project?</h2>
             <p class="text-xl mb-6 max-w-3xl mx-auto">Let's work together to create something amazing.</p>
