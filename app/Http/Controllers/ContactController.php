e34b43b92cbc197dbe6d839156dbbe29cdf1d6ab
Ciiruu <ricardho.gunawan@gmail.com>
2025-03-07 17:22:04 +0700

    .

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -14,7 +14,7 @@ class ContactController extends Controller
      */
     public function index()
     {
-        return view('emails.contact');
+        return view('email.contact');
     }
 
     /**
